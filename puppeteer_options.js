d7a855dffe36d24813e666038c0541e355e086ae
timzprof <timilehin.olumofin@gmail.com>
2018-11-21 11:19:11 +0100

    Added All Other Female Hostel ID to Halls

--- puppeteer_options.js
+++ puppeteer_options.js
@@ -6,5 +6,5 @@ module.exports = {
     disabledResources: ['stylesheet', 'media', 'font', 'script', 'image'],
     userDataDir: "./data",
     cacheEnabled: true,
-    halls: ["MOREMI+HALL", 'HONOURS+HALL']
+    halls: ["MOREMI+HALL", 'HONOURS+HALL', 'KOFO+ADEMOLA+HALL', 'MADAM+TINUBU+HALL', 'QUEEN+AMINA+HALL', 'FAGUNWA+HALL','ALIYU+MAKAMA+BIDA+HALL']
 };
