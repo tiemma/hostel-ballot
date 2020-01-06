c43052d353ed8f1a7b86ab12edbf472292ce0490
timzprof <timilehin.olumofin@gmail.com>
2020-01-06 18:40:49 +0100

    Changed Link to New Accommodation URL

--- index.js
+++ index.js
@@ -9,9 +9,6 @@ console.log(options);
 
 let delay = 0;
 
-// for(let i = 0; i < 4; i++)
-// {
-//     setTimeout(
 (async () => {
     const browser = await puppeteer.launch({
         timeout: options.timeout, headless: options.headless, userDataDir: `${options.userDataDir}-${Math.floor(Math.random() * 4)}`, args: [
@@ -22,27 +19,11 @@ let delay = 0;
 
     let page = await browser.newPage();
 
-    // await page.setRequestInterception(true);
-    //
-    // page.on('request', (req) => {
-    //     console.log("url for this request" + req.url());
-    //
-    //     if (options.disabledResources.indexOf(req.resourceType()) !== -1) {
-    //         req.abort();
-    //     } else if (req.url().includes("HomePage")) {
-    //         window.open(req.url(), '_blank');
-    //         console.log("Home page found~");
-    //     } else {
-    //         req.continue();
-    //     }
-    // });
-
     await page.setJavaScriptEnabled(options.javascriptEnabled);
 
     await page.setCacheEnabled(options.cacheEnabled);
 
-    await page.goto('http://studentportal.unilag.edu.ng/', {timeout: options.timeout});
-    // await page.goto('http://webcache.googleusercontent.com/search?q=cache:http://studentportal.unilag.edu.ng/', {timeout: options.timeout});
+    await page.goto('http://accommodation.unilag.edu.ng/', {timeout: options.timeout});
 
     await page.click(fields.LOGIN_USERNAME_ID);
     await page.keyboard.type(process.env.MATRIC_NO);
@@ -98,12 +79,7 @@ let delay = 0;
         }, delay+=10000);
     }
 
-    // }
-
-    // }
-    // browser.close();
 })();
-// , delay += 6000 * i);
 
 
 
